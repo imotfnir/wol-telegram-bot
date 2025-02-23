5f34123bec6d0cdf25196a4b8f1dae22ac2f77b2
rigel_pang <imotfnir@gmail.com>
2025-02-23 10:10:54 +0800

    fix: compatibility issue, downgrade to python version 3.12

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
-FROM python:3.14-rc-alpine3.21
+FROM python:3.12-alpine
 WORKDIR /app
 COPY wol.py /app/wol.py
-CMD ["python", "/app/wol.py"]
+COPY requirements.txt /app/requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+CMD ["pip" , "list python-telegram-bot"]
+CMD ["/bin/sh"]
+CMD ["tail", "-f", "/dev/null"]
+# CMD ["python", "/app/wol.py"]
+
--- wol.py
+++ wol.py
@@ -1,25 +1,24 @@
 import os
-from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 10000
 BROADCAST_IP = "192.168.0.255"
-
 MAC_TABLE = {
     "rigel": "04:D9:F5:F3:D0:67",
     "dell": "00:D4:9E:9B:1E:A5",
     "paul": "60:45:CB:9D:AC:6B",
     "paullt": "A0:80:69:F7:1E:1C"
 }
-
-TOKEN = "7830008810:AAGeink0jwQ5XZoRpS4DoLe7eIgAYePo9aU"
+TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+if not TOKEN:
+    raise ValueError("Error: TELEGRAM_BOT_TOKEN environment variable not set")
 app = Application.builder().token(TOKEN).build()
 
-async def start(update: Update, context):
+async def start(update, context):
     await update.message.reply_text("Please enter the name of the device to be woken up, for example: /wol mypc\nCurrently supported are rigel, dell, paul")
 
-async def wol(update: Update, context):
+async def wol(update, context):
     if not context.args:
         await update.message.reply_text("Please enter the name of the device to be woken up, for example: /wol dell")
         return
@@ -31,7 +30,7 @@ async def wol(update: Update, context):
         return
 
     mac_address = MAC_TABLE[device_name]
-    os.system("wakeonlan -i {} {}".format(BROADCAST_IP, mac_address))
+    os.system(f"wakeonlan -i {BROADCAST_IP} {mac_address}")
     await update.message.reply_text(f"Sending WOL to {mac_address}")
     return
 
@@ -39,5 +38,5 @@ async def wol(update: Update, context):
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("wol", wol))
 
-app.run_polling()
-
+if __name__ == "__main__":
+    app.run_polling()
