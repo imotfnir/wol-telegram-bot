274002671b22b27ec63e74756f19177d490ef487
rigel_pang <imotfnir@gmail.com>
2025-02-23 14:34:37 +0800

    fix: use socket to send magic package, instead of wakeonlan command

--- wol.py
+++ wol.py
@@ -1,4 +1,5 @@
 import os
+import socket
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 
 UDP_IP = "0.0.0.0"
@@ -15,23 +16,34 @@ if not TOKEN:
     raise ValueError("Error: TELEGRAM_BOT_TOKEN environment variable not set")
 app = Application.builder().token(TOKEN).build()
 
+
+def send_wol(hostname):
+    mac_address=MAC_TABLE[hostname]
+    mac_bytes = bytes.fromhex(mac_address.replace(":", "").replace("-", ""))
+    magic_packet = b'\xff' * 6 + mac_bytes * 16
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        s.sendto(magic_packet, (BROADCAST_IP, 9))
+
+
 async def start(update, context):
     await update.message.reply_text("Please enter the name of the device to be woken up, for example: /wol mypc\nCurrently supported are rigel, dell, paul")
+
 
 async def wol(update, context):
     if not context.args:
         await update.message.reply_text("Please enter the name of the device to be woken up, for example: /wol dell")
         return
 
-    device_name = context.args[0].lower()
+    hostname = context.args[0].lower()
 
-    if device_name not in MAC_TABLE:
-        await update.message.reply_text(f"Device {device_name} not found, please make sure the name is correct!")
+    if hostname not in MAC_TABLE:
+        await update.message.reply_text(f"Device {hostname} not found, please make sure the name is correct!")
         return
 
-    mac_address = MAC_TABLE[device_name]
-    os.system(f"wakeonlan -i {BROADCAST_IP} {mac_address}")
-    await update.message.reply_text(f"Sending WOL to {mac_address}")
+    send_wol(hostname)
+    await update.message.reply_text(f"Sending WOL to {MAC_TABLE[hostname]}")
     return
 
 
